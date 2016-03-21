ea5c8fc440455d48091cb989c8f2497a9c409171
joshua levin <joshua.c.levin@gmail.com>
2016-03-21 08:37:53 +0000

    add more free assets, create a test level and get the character running around it and not through things

---
+++ Glitch/Assets/Scripts/Views/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	Transform player;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+	}
+
+	void Update ()
+	{
+		transform.position = new Vector3 (player.position.x + 4, transform.position
+			.y, player.position.z);
+	}
+}
---
+++ Glitch/Assets/Scripts/Views/RootMotionScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Animator))]
+
+public class RootMotionScript : MonoBehaviour
+{
+	Animator animator;
+
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnAnimatorMove ()
+	{
+		float movement = (Mathf.Abs (Input.GetAxis ("Vertical")) > Mathf.Abs (Input.GetAxis ("Horizontal"))) ? Mathf.Abs (Input.GetAxis ("Vertical")) : Mathf.Abs (Input.GetAxis ("Horizontal"));
+
+		Rigidbody r = GetComponent<Rigidbody> ();
+		r.velocity = new Vector3 (-Input.GetAxis ("Vertical") * 3, 0, Input.GetAxisRaw ("Horizontal") * 3);
+
+		animator.SetFloat ("MoveForward", movement);
+
+		Vector3 lookPos = new Vector3 (-Input.GetAxisRaw ("Vertical"), 0, Input.GetAxisRaw ("Horizontal"));
+
+		if (lookPos != Vector3.zero)
+			transform.localRotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (lookPos), Time.deltaTime * 10);
+
+		//transform.localPosition += transform.forward * animator.GetFloat ("MoveForward") * Time.deltaTime * 3;
+	}
+}
